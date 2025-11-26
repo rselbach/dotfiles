a8f3a4a4341eea2ff1ec66335f239c30d4d682d1
Roberto Selbach <rselbach@rselbach.com>
2025-11-26 18:31:12 -0500

    nvim: cleanup config, switch to supermaven, track lazy-lock
    - remove copilot (using supermaven instead)
    - track lazy-lock.json for reproducibility
    - remove empty autocmds.lua
    - remove redundant gopls hints config
    - add scrolloff=8
    - enable json/yaml/markdown lang extras

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 --
 vim.opt.wrap = true
+vim.opt.scrolloff = 8
 -- vim.g.codeium_os = "Darwin"
 -- vim.g.codeium_arch = "arm64"
 vim.g.codeium_enabled = false
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,15 +19,6 @@ return {
                 upgrade_dependency = true,
                 vendor = true,
               },
-              hints = {
-                assignVariableTypes = false,
-                compositeLiteralFields = false,
-                compositeLiteralTypes = false,
-                constantValues = false,
-                functionTypeParameters = false,
-                parameterNames = false,
-                rangeVariableTypes = false,
-              },
               analyses = {
                 fieldalignment = true,
                 nilness = true,
