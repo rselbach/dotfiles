d2ff5443fa16ae1332371eca49f0ef27bc6f4416
Roberto Selbach <rselbach@rselbach.com>
2026-01-10 21:28:04 -0500

    switch back to catppuccin

--- fish/config.fish
+++ fish/config.fish
@@ -212,6 +212,16 @@ fish_add_path $HOME/.opencode/bin
 # gpg
 set -gx GPG_TTY (tty)
 
+# Catppuccin Mocha FZF colors
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a"
+
+# bat theme
+set -gx BAT_THEME "Catppuccin Mocha"
+
 # fnm (Fast Node Manager) - lazy loaded on first node/npm/npx call
 if type -q fnm; and not functions -q __fnm_initialized
     function node --wraps node
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,34 @@
 return {
   {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     lazy = false,
-    config = function()
-      vim.opt.background = "light"
-      vim.cmd.colorscheme("zenbones")
+    opts = {
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        telescope = { enabled = true },
+        which_key = true,
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
     end,
-  }
+  },
 }
