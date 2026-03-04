cf294d5ef4c4794367fb335c26cbbc3e5adcfdbd
Roberto Selbach <rselbach@rselbach.com>
2026-03-04 10:10:07 -0500

    Solarized light

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,13 +4,27 @@ if vim.uv.fs_stat(omarchy) then
   return dofile(omarchy)
 end
 
--- fallback when omarchy is not available
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  "maxmx03/solarized.nvim",
+  lazy = false,
+  priority = 1000,
+  ---@type solarized.config
+  opts = {},
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "light"
+    require("solarized").setup(opts)
+    vim.cmd.colorscheme("solarized")
+  end,
 }
+
+-- fallback when omarchy is not available
+--return {
+--  { "ellisonleao/gruvbox.nvim" },
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "gruvbox",
+--    },
+--  },
+--}
