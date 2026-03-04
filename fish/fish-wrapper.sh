6424dd62a2b919bd3d2d283974dcd35eea9ef22a
Roberto Selbach <rselbach@rselbach.com>
2026-03-04 06:46:51 -0500

    Remove unused configs

--- fish/fish-wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$(uname)" = "Darwin" ]; then
-    exec /opt/homebrew/bin/fish "$@"
-else
-    exec /home/linuxbrew/.linuxbrew/bin/fish "$@"
-fi
--- i3/monitor-resolution.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xev -root -event randr | grep --line-buffered "subtype XRROutputChangeNotifyEvent" | while read line; do
-	xrandr --output $(xrandr | grep " connected" | cut -f1 -d " ") --auto
-done
