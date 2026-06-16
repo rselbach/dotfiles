eb5a52dd2356c6fb8ed377ba71ce2ee4048f36f0
Roberto Selbach <rselbach@rselbach.com>
2026-06-15 20:04:13 -0400

    Small fixes

--- zsh/rc.d/17-homebrew.zsh
+++ zsh/rc.d/17-homebrew.zsh
@@ -1,4 +1,21 @@
-path+=(
-  /opt/homebrew/bin
-  /opt/homebrew/sbin
-)
+{
+  local _brew_prefix
+
+  if [[ -x /opt/homebrew/bin/brew ]]; then
+    _brew_prefix=/opt/homebrew
+  elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    _brew_prefix=/home/linuxbrew/.linuxbrew
+  elif [[ -x ~/.linuxbrew/bin/brew ]]; then
+    _brew_prefix=~/.linuxbrew
+  elif [[ -x /usr/local/bin/brew ]]; then
+    _brew_prefix=/usr/local
+  fi
+
+  if [[ -n "${_brew_prefix}" ]]; then
+    path+=(
+      "${_brew_prefix}/bin"
+      "${_brew_prefix}/sbin"
+    )
+    export HOMEBREW_PREFIX="${_brew_prefix}"
+  fi
+}
--- zsh/rc.d/18-fnm.zsh
+++ zsh/rc.d/18-fnm.zsh
@@ -1,4 +1,29 @@
-path+=("/opt/homebrew/opt/fnm/bin")
+{
+  local _fnm_path
+
+  if [[ -n "${HOMEBREW_PREFIX:-}" && -d "${HOMEBREW_PREFIX}/opt/fnm/bin" ]]; then
+    _fnm_path="${HOMEBREW_PREFIX}/opt/fnm/bin"
+  else
+    local -a _fnm_candidates=(
+      /opt/homebrew/opt/fnm/bin
+      /home/linuxbrew/.linuxbrew/opt/fnm/bin
+      ~/.linuxbrew/opt/fnm/bin
+      /usr/local/opt/fnm/bin
+    )
+    local p
+    for p in "${_fnm_candidates[@]}"; do
+      if [[ -d "${p}" ]]; then
+        _fnm_path="${p}"
+        break
+      fi
+    done
+    unset _fnm_candidates p
+  fi
+
+  if [[ -n "${_fnm_path}" ]]; then
+    path+=("${_fnm_path}")
+  fi
+}
 
 if command -v fnm >/dev/null 2>&1; then
   _fnm_lazy_env() {
