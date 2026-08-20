3f0270269fc6c5b25448a6369ef39b9953aee0d2
Roberto Selbach <rselbach@rselbach.com>
2026-08-19 20:20:41 -0400

    Rounded corners

--- hypr/callisto/looknfeel.lua
+++ hypr/callisto/looknfeel.lua
@@ -14,16 +14,16 @@
 -- })
 
 -- https://wiki.hypr.land/Configuring/Basics/Variables/#decoration
--- hl.config({
---   decoration = {
---     -- Use round window corners.
---     rounding = 8,
---
---     -- Dim unfocused windows (0.0 = no dim, 1.0 = fully dimmed).
---     dim_inactive = true,
---     dim_strength = 0.15,
---   },
--- })
+hl.config({
+	decoration = {
+		-- Use round window corners.
+		rounding = 8,
+
+		-- Dim unfocused windows (0.0 = no dim, 1.0 = fully dimmed).
+		dim_inactive = true,
+		dim_strength = 0.15,
+	},
+})
 
 -- https://wiki.hypr.land/Configuring/Basics/Variables/#animations
 -- hl.config({
